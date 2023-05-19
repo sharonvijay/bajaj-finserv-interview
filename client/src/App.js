811cfa572dda2ac2bc8de52107bc53f408f29d3a
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-05-19 19:49:03 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import EmployeeTable from "../src/components/EmployeeTable";
 
-function App() {
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    // Fetch JSON data from the API or local file
+    fetch("http://localhost:5000/api/employees")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data.employees);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Employee Table</h1>
+      <EmployeeTable employees={employees} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/EmployeeTable.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { DataGrid, GridToolbarContainer, GridToolbarColumnsButton,} from '@mui/x-data-grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, Switch, FormControlLabel } from '@mui/material';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const EmployeeTable = ({ employees }) => {
+  const [filteredRows, setFilteredRows] = useState(employees);
+  const [filterName, setFilterName] = useState('');
+  const [filterDesignation, setFilterDesignation] = useState('');
+  const [filterSkills, setFilterSkills] = useState('');
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+
+  const handleFilterByName = (event) => {
+    const name = event.target.value.toLowerCase();
+    setFilterName(name);
+    applyFilters(name, filterDesignation, filterSkills);
+  };
+
+  const handleFilterByDesignation = (event) => {
+    const designation = event.target.value;
+    setFilterDesignation(designation);
+    applyFilters(filterName, designation, filterSkills);
+  };
+
+  const handleFilterBySkills = (event) => {
+    const skills = event.target.value.toLowerCase();
+    setFilterSkills(skills);
+    applyFilters(filterName, filterDesignation, skills);
+  };
+
+  const applyFilters = (name, designation, skills) => {
+    const filteredData = employees.filter((employee) =>
+      employee.name && employee.name.toLowerCase().includes(name) &&
+      (designation === '' || employee.designation === designation) &&
+      (skills === '' || (employee.skills && employee.skills.some((skill) => skill.toLowerCase().includes(skills))))
+    );
+    setFilteredRows(filteredData);
+  };
+
+  const handleThemeChange = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'designation', headerName: 'Designation', width: 150 },
+    { field: 'skills', headerName: 'Skills', width: 250 },
+  ];
+
+  const CustomToolbar = () => {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton />
+\        <FormControlLabel
+          control={<Switch checked={isDarkTheme} onChange={handleThemeChange} />}
+          label="Dark Theme"
+        />
+      </GridToolbarContainer>
+    );
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ width: '80%' }}>
+          <input
+            type="text"
+            placeholder="Search by Name"
+            onChange={handleFilterByName}
+          />
+
+          <select value={filterDesignation} onChange={handleFilterByDesignation}>
+            <option value="">All Designations</option>
+            <option value="Senior Developer">Senior Developer</option>
+            <option value="QA Engineer">QA Engineer</option>
+            <option value="Project Manager">Project Manager</option>
+          </select>
+
+          <input
+            type="text"
+            placeholder="Search by Skills"
+            onChange={handleFilterBySkills}
+          />
+        </div>
+
+        <DataGrid
+          rows={filteredRows}
+          columns={columns}
+          components={{
+            Toolbar: CustomToolbar,
+          }}
+        />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default EmployeeTable;
